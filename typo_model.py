fa658eb4ca04b72feae0266d0b4454c5b76adf54
Filipp Bakanov <filipp@bakanov.su>
2017-11-12 23:31:33 +0300

    Added simple typo model

---
+++ typo_model.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+import random
+import bisect
+from scipy.stats import binom
+
+#todo: calculate correct typo probabilities
+
+TYPO_PROB = 0.02 # chance of making typo for a single letter
+TYPO_COUNT_PROB = [0.8, 0.15, 0.04, 0.01] # chance of making multiple typos
+REPLACE_PROB = 0.7
+INSERT_PROB = 0.1
+REMOVE_PROB = 0.1
+TRANSPOSE_PROB = 0.1
+TRANSPOSE_DISTANCE_PROB = [0.8, 0.15, 0.04, 0.01]
+EPSILON = 0.001
+
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+assert 1.0 >= TYPO_PROB > 0
+assert abs(sum(TYPO_COUNT_PROB) - 1.0) < EPSILON
+assert abs(REPLACE_PROB + INSERT_PROB + REMOVE_PROB + TRANSPOSE_PROB - 1.0) < EPSILON
+
+# Randomly selects a value from list [(value, weight), ... ]
+def weightedChoice(values):
+    values, weights = zip(*values)
+    totalWeight = sum(weights)
+    sumWeight = 0.
+    distribution = []
+    for w in weights:
+        sumWeight += w
+        distribution.append(sumWeight / totalWeight)
+    return values[bisect.bisect(distribution, random.random())]
+
+def typoReplace(word):
+    if not word:
+        return word
+    l = random.randint(0, len(word) - 1)
+    return word[:l] + random.choice(ALPHABET) + word[l + 1:]
+
+def typoInsert(word):
+    l = random.randint(0, len(word))
+    return word[:l] + random.choice(ALPHABET) + word[l:]
+
+def typoRemove(word):
+    if not word:
+        return word
+    l = random.randint(0, len(word) - 1)
+    return word[:l] + word[l + 1:]
+
+def swapLetter(s, i, j):
+    lst = list(s)
+    lst[i], lst[j] = lst[j], lst[i]
+    return ''.join(lst)
+
+def typoTranspose(word):
+    l = random.randint(0, len(word) - 1)
+    d = weightedChoice(enumerate(TRANSPOSE_DISTANCE_PROB)) + 1
+    l1 = max(0, l - d / 2)
+    l2 = min(len(word)-1, l1 + d)
+    return swapLetter(word, l1, l2)
+
+
+TYPO_TYPES = [REPLACE_PROB, INSERT_PROB, REMOVE_PROB, TRANSPOSE_PROB]
+TYPO_GENERATORS = [typoReplace, typoInsert, typoRemove, typoTranspose]
+
+def getWordTypoChance(word):
+    l = len(word)
+    noTypoChance = 1.0 - TYPO_PROB
+    wordNoTypoChance = binom.pmf(l, l, noTypoChance)
+    return 1.0 - wordNoTypoChance
+
+def generateTypo(word):
+    if random.random() > getWordTypoChance(word):
+        return word
+
+    typos = weightedChoice(enumerate(TYPO_COUNT_PROB)) + 1
+
+    for i in xrange(typos):
+        typoType = weightedChoice(enumerate(TYPO_TYPES))
+        word = TYPO_GENERATORS[typoType](word)
+
+    return word
+
