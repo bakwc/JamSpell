04c2fa2aaf065707a5c0b974e7beec72816168ab
Filipp Bakanov <filipp@bakanov.su>
2017-12-25 23:56:30 +0300

    Add python bindings with swig

--- openspell/spell_corrector.hpp
+++ openspell/spell_corrector.hpp
@@ -8,17 +8,17 @@ namespace NOpenSpell {
 class TSpellCorrector {
 public:
     bool LoadLangModel(const std::string& modelFile);
-    TWords GetCandidatesRaw(const TWords& sentence, size_t position) const;
+    NOpenSpell::TWords GetCandidatesRaw(const NOpenSpell::TWords& sentence, size_t position) const;
     std::vector<std::wstring> GetCandidates(const std::vector<std::wstring>& sentence, size_t position) const;
     std::wstring FixFragment(const std::wstring& text) const;
     std::wstring FixFragmentNormalized(const std::wstring& text) const;
 private:
-    TWords Edits(const TWord& word, bool lastLevel = true) const;
-    TWords Edits2(const TWord& word, bool lastLevel = true) const;
+    NOpenSpell::TWords Edits(const NOpenSpell::TWord& word, bool lastLevel = true) const;
+    NOpenSpell::TWords Edits2(const NOpenSpell::TWord& word, bool lastLevel = true) const;
 private:
     TLangModel LangModel;
-    std::unordered_map<std::wstring, std::unordered_set<TWord, TWordHashPtr>> Deletes1;
-    std::unordered_map<std::wstring, std::unordered_set<TWord, TWordHashPtr>> Deletes2;
+    std::unordered_map<std::wstring, std::unordered_set<NOpenSpell::TWord, TWordHashPtr>> Deletes1;
+    std::unordered_map<std::wstring, std::unordered_set<NOpenSpell::TWord, TWordHashPtr>> Deletes2;
 };
 
 
---
+++ swig/openspell.i
@@ -0,0 +1,7 @@
+%module openspell
+%include <std_string.i>
+%include <std_wstring.i>
+%{
+#include "openspell/spell_corrector.hpp"
+%}
+%include "openspell/spell_corrector.hpp"
