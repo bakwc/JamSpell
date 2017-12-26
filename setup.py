bfe2228f53b68cb7bdaec9005f241bbb579f6e26
Filipp Bakanov <filipp@bakanov.su>
2017-12-26 23:47:30 +0300

    Added test setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+from setuptools.extension import Extension
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+
+openspell = Extension(
+    name='openspell',
+    include_dirs=[this_dir],
+    sources=[
+        os.path.join(this_dir, 'openspell', 'lang_model.cpp'),
+        os.path.join(this_dir, 'openspell', 'spell_corrector.cpp'),
+        os.path.join(this_dir, 'openspell', 'utils.cpp'),
+        os.path.join(this_dir, 'swig', 'openspell.i')
+    ],
+    extra_compile_args=['-std=c++11'],
+    swig_opts=['-c++'],
+)
+
+setup(
+    name='openspell',
+    version='0.0.1',
+    author='Filipp Ozinov',
+    description='spell checker',
+    url='http://example.com',
+    classifiers=[
+        # The list of PyPI classifiers
+    ],
+    #ext_modules=[openspell_cpp, openspell],
+    ext_modules=[openspell],
+    zip_safe=False,
+    include_package_data=True,
+)
