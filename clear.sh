3fe55e8b419e977eb3f6df9812c56565bcc596ea
Filipp Bakanov <filipp@bakanov.su>
2018-01-16 22:40:38 +0300

    train -> main renamed

--- clear.sh
+++ clear.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rm -rf jamspell.egg-info/ dist/ build/ jamspell.py jamspell.egg-info/ jamspell_wrap.cpp _jamspell.so
+rm -rf jamspell.egg-info/ dist/ build/ jamspell.py jamspell.egg-info/ jamspell_wrap.cpp _jamspell.so .cache
