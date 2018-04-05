5d0d41c2e5ffd67b983419fa5806ed5b140b8d70
den <dennis.victorovich@gmail.com>
2018-04-05 14:02:10 +0300

    add build option to use boost.locale instead of std::codecvt to be able to compile on GCC < 5

--- jamspell/utils.cpp
+++ jamspell/utils.cpp
@@ -1,11 +1,16 @@
 #include <fstream>
 #include <sstream>
-#include <codecvt>
 #include <chrono>
 #include <cassert>
 #include <iostream>
 #include <cassert>
 #include <algorithm>
+
+#ifdef USE_BOOST_CONVERT
+    #include <boost/locale/encoding_utf.hpp>
+#else
+    #include <codecvt>
+#endif
 
 #include "utils.hpp"
 
@@ -99,14 +104,24 @@ const std::unordered_set<wchar_t>& TTokenizer::GetAlphabet() const {
 }
 
 std::wstring UTF8ToWide(const std::string& text) {
+#ifdef USE_BOOST_CONVERT
+    using boost::locale::conv::utf_to_utf;
+    return utf_to_utf<wchar_t>(text.c_str(), text.c_str() + text.size());
+#else
     std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-    return converter.from_bytes(text);;
+    return converter.from_bytes(text);
+#endif
 }
 
 std::string WideToUTF8(const std::wstring& text) {
+#ifdef USE_BOOST_CONVERT
+    using boost::locale::conv::utf_to_utf;
+    return utf_to_utf<char>(text.c_str(), text.c_str() + text.size());
+#else
     using convert_type = std::codecvt_utf8<wchar_t>;
     std::wstring_convert<convert_type, wchar_t> converter;
     return converter.to_bytes(text);
+#endif
 }
 
 uint64_t GetCurrentTimeMs() {
