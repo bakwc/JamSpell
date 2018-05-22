70d1d2e456d02c9fdf8c095f738eef33f7244de4
Ivan Osadchiy <ivan.osadchii@gmail.com>
2018-05-22 09:25:57 +0300

    WIP Add 'load_ngrams' command

--- main/main.cpp
+++ main/main.cpp
@@ -8,6 +8,7 @@ using namespace NJamSpell;
 void PrintUsage(const char** argv) {
     std::cerr << "Usage: " << argv[0] << " mode args" << std::endl;
     std::cerr << "    train alphabet.txt dataset.txt resultModel.bin  - train model" << std::endl;
+    std::cerr << "    load_ngrams alphabet.txt grams1.csv grams2.csv grams3.csv resultModel.bin  - load model from 1-, 2-, and 3-grams" << std::endl;
     std::cerr << "    score model.bin - input sentences and get score" << std::endl;
     std::cerr << "    correct model.bin - input sentences and get corrected one" << std::endl;
     std::cerr << "    fix model.bin input.txt output.txt - automatically fix txt file" << std::endl;
@@ -19,6 +20,18 @@ int Train(const std::string& alphabetFile,
 {
     TLangModel model;
     model.Train(datasetFile, alphabetFile);
+    model.Dump(resultModelFile);
+    return 0;
+}
+
+int LoadNGrams(const std::string& alphabetFile,
+                const std::string& gram1File,
+                const std::string& gram2File,
+                const std::string& gram3File,
+          const std::string& resultModelFile)
+{
+    TLangModel model;
+    model.LoadNGrams(gram1File, gram2File, gram3File, alphabetFile);
     model.Dump(resultModelFile);
     return 0;
 }
@@ -93,6 +106,17 @@ int main(int argc, const char** argv) {
         std::string datasetFile = argv[3];
         std::string resultModelFile = argv[4];
         return Train(alphabetFile, datasetFile, resultModelFile);
+    } else if (mode == "load_ngrams") {
+        if (argc < 7) {
+            PrintUsage(argv);
+            return 42;
+        }
+        std::string alphabetFile = argv[2];
+        std::string gram1File = argv[3];
+        std::string gram2File = argv[4];
+        std::string gram3File = argv[5];
+        std::string resultModelFile = argv[6];
+        return LoadNGrams(alphabetFile, gram1File, gram2File, gram3File, resultModelFile);
     } else if (mode == "score") {
         if (argc < 3) {
             PrintUsage(argv);
