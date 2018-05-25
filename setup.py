85c4e9f278a91bb15ac958a5efc5ff3b98041e5c
Filipp Ozinov <filipp@bakanov.su>
2018-05-25 18:38:36 +0300

    New release for pip

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ class Swig3Ext(build_ext):
         assert subprocess.check_output([swigBinary, "-version"]).find(b'SWIG Version 3') != -1
         return swigBinary
 
-VERSION = '0.0.10'
+VERSION = '0.0.11'
 
 setup(
     name='jamspell',
