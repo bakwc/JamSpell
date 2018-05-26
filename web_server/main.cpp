8f2f744492bcbc228dcb62aac9654e3be7d91e40
Filipp Ozinov <filipp@bakanov.su>
2018-05-26 11:58:14 +0300

    little refactor

--- web_server/main.cpp
+++ web_server/main.cpp
@@ -2,6 +2,54 @@
 #include "contrib/httplib/httplib.h"
 #include "contrib/nlohmann/json.hpp"
 
+std::string GetCandidates(const NJamSpell::TSpellCorrector& corrector,
+                          const std::string& text)
+{
+    std::wstring input = NJamSpell::UTF8ToWide(text);
+    std::transform(input.begin(), input.end(), input.begin(), std::towlower);
+    NJamSpell::TSentences sentences = corrector.GetLangModel().Tokenize(input);
+
+    nlohmann::json results;
+    results["results"] = nlohmann::json::array();
+
+    for (size_t i = 0; i < sentences.size(); ++i) {
+        const NJamSpell::TWords& sentence = sentences[i];
+        for (size_t j = 0; j < sentence.size(); ++j) {
+            NJamSpell::TWord currWord = sentence[j];
+            std::wstring wCurrWord(currWord.Ptr, currWord.Len);
+            NJamSpell::TWords candidates = corrector.GetCandidatesRaw(sentence, j);
+            if (candidates.empty()) {
+                continue;
+            }
+            std::wstring firstCandidate(candidates[0].Ptr, candidates[0].Len);
+            if (wCurrWord == firstCandidate) {
+                continue;
+            }
+            nlohmann::json currentResult;
+            currentResult["pos_from"] = currWord.Ptr - &input[0];
+            currentResult["len"] = currWord.Len;
+            currentResult["candidates"] = nlohmann::json::array();
+
+            size_t candidatesSize = std::min(candidates.size(), size_t(7));
+            for (size_t k = 0; k < candidatesSize; ++k) {
+                NJamSpell::TWord candidate = candidates[k];
+                std::string candidateStr = NJamSpell::WideToUTF8(std::wstring(candidate.Ptr, candidate.Len));
+                currentResult["candidates"].push_back(candidateStr);
+            }
+
+            results["results"].push_back(currentResult);
+        }
+    }
+
+    return results.dump(4);
+}
+
+std::string FixText(const NJamSpell::TSpellCorrector& corrector,
+                    const std::string& text)
+{
+    std::wstring input = NJamSpell::UTF8ToWide(text);
+    return NJamSpell::WideToUTF8(corrector.FixFragment(input));
+}
 
 int main(int argc, const char** argv) {
     if (argc != 3) {
@@ -21,49 +69,11 @@ int main(int argc, const char** argv) {
 
     httplib::Server srv;
     srv.Get("/fix", [&corrector](const httplib::Request& req, httplib::Response& resp) {
-        std::wstring input = NJamSpell::UTF8ToWide(req.get_param_value("text"));
-        std::string result = NJamSpell::WideToUTF8(corrector.FixFragment(input));
-        resp.set_content(result, "text/plain");
+        resp.set_content(FixText(corrector, req.get_param_value("text")), "text/plain");
     });
 
-    srv.Get("/fix_candidates", [&corrector](const httplib::Request& req, httplib::Response& resp) {
-        std::wstring input = NJamSpell::UTF8ToWide(req.get_param_value("text"));
-        std::transform(input.begin(), input.end(), input.begin(), std::towlower);
-        NJamSpell::TSentences sentences = corrector.GetLangModel().Tokenize(input);
-
-        nlohmann::json results;
-        results["results"] = nlohmann::json::array();
-
-        for (size_t i = 0; i < sentences.size(); ++i) {
-            const NJamSpell::TWords& sentence = sentences[i];
-            for (size_t j = 0; j < sentence.size(); ++j) {
-                NJamSpell::TWord currWord = sentence[j];
-                std::wstring wCurrWord(currWord.Ptr, currWord.Len);
-                NJamSpell::TWords candidates = corrector.GetCandidatesRaw(sentence, j);
-                if (candidates.empty()) {
-                    continue;
-                }
-                std::wstring firstCandidate(candidates[0].Ptr, candidates[0].Len);
-                if (wCurrWord == firstCandidate) {
-                    continue;
-                }
-                nlohmann::json currentResult;
-                currentResult["pos_from"] = currWord.Ptr - &input[0];
-                currentResult["len"] = currWord.Len;
-                currentResult["candidates"] = nlohmann::json::array();
-
-                size_t candidatesSize = std::min(candidates.size(), size_t(7));
-                for (size_t k = 0; k < candidatesSize; ++k) {
-                    NJamSpell::TWord candidate = candidates[k];
-                    std::string candidateStr = NJamSpell::WideToUTF8(std::wstring(candidate.Ptr, candidate.Len));
-                    currentResult["candidates"].push_back(candidateStr);
-                }
-
-                results["results"].push_back(currentResult);
-            }
-        }
-
-        resp.set_content(results.dump(4), "text/plain");
+    srv.Get("/candidates", [&corrector](const httplib::Request& req, httplib::Response& resp) {
+        resp.set_content(GetCandidates(corrector, req.get_param_value("text")), "text/plain");
     });
 
     std::cerr << "[info] starting web server at localhost:" << port << std::endl;
